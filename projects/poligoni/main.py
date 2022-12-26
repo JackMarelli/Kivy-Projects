ccfd867c6350da348723fa4d78820cf6ea233407
JackMarelli <marelligiacomo@gmail.com>
2022-12-26 14:15:43 +0100

    test commit

--- projects/poligoni/main.py
+++ projects/poligoni/main.py
@@ -1,13 +1,8 @@
 import math
 
-import kivy
 from kivy.lang import Builder
-from kivy.uix.accordion import Accordion, AccordionItem
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
+from kivy.uix.accordion import Accordion
 from kivy.app import App
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
 
 Builder.load_file("all.kv")
 
