f6abc9c1283b1789fb879f5cc09157ed3bab1257
JackMarelli <marelligiacomo@gmail.com>
2023-01-02 19:12:59 +0100

    fonts and title label

--- projects/ToDoList/main.py
+++ projects/ToDoList/main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.core.text import LabelBase
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.config import Config
@@ -7,6 +8,8 @@ Config.set('graphics', 'width', '375')
 Config.set('graphics', 'height', '812')
 
 Builder.load_file("main.kv")
+LabelBase.register(name='Montserrat', fn_regular='fonts/Montserrat-VariableFont_wght.ttf')
+LabelBase.register(name='Montserrat_Italic', fn_regular='fonts/Montserrat-Italic-VariableFont_wght.ttf')
 
 class Task():
     deadline = None
